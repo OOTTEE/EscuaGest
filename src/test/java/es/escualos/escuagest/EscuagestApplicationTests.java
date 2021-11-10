b1007950df675d0db03ae77058d28e5b94ff5272
Nelson <Nelsonshadow@yahoo.es>
2021-11-10 17:43:17 +0100

    Arranque proyecto

---
+++ src/test/java/es/escualos/escuagest/EscuagestApplicationTests.java
@@ -0,0 +1,13 @@
+package es.escualos.escuagest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EscuagestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
