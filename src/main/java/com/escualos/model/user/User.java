763b7784cc0ad81d740781dc906908cc2e5b3a06
Javier (OTE) <javier.ote@protonmail.com>
2022-04-08 10:43:19 +0200

    Fix mongodb connection

---
+++ src/main/java/com/escualos/model/user/User.java
@@ -0,0 +1,16 @@
+package com.escualos.model.user;
+
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+@Builder
+@Data
+public class User {
+
+    private String id;
+    private String username;
+    private String fullname;
+
+}
---
+++ src/main/java/com/escualos/rest/UserApiDelegateImpl.java
@@ -0,0 +1,46 @@
+package com.escualos.rest;
+
+import com.escualos.api.UserApiDelegate;
+import com.escualos.model.CreateUserRequest;
+import com.escualos.model.UserResponse;
+import com.escualos.model.user.User;
+import com.escualos.model.user.UserRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.net.URI;
+
+@Component
+public class UserApiDelegateImpl implements UserApiDelegate {
+
+    private UserRepository userRepository;
+
+    public UserApiDelegateImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public Mono<ResponseEntity<UserResponse>> getUserByName(String username, ServerWebExchange exchange) {
+        return UserApiDelegate.super.getUserByName(username, exchange);
+    }
+
+    @Override
+    public Mono<ResponseEntity<UserResponse>> getUserById(String id, ServerWebExchange exchange) {
+        return UserApiDelegate.super.getUserById(id, exchange);
+    }
+
+
+    @Override
+    public Mono<ResponseEntity<UserResponse>> postNewUser(Mono<CreateUserRequest> createUserRequest, ServerWebExchange exchange) {
+        return createUserRequest
+            .map(createUser -> User.builder()
+                    .fullname(createUser.getFullname())
+                    .username(createUser.getUsername())
+                    .build()
+            )
+            .flatMap(userRepository::save)
+            .map(user -> ResponseEntity.created(URI.create("/user/%s".formatted(user.getId()))).build());
+    }
+}
