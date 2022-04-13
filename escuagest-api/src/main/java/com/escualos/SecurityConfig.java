20a03422544a47d012841fa15e07431ca39c026a
Javier (OTE) <javier.ote@protonmail.com>
2022-04-13 15:23:54 +0200

    Enable jwt roles, and method control security

--- escuagest-api/src/main/java/com/escualos/SecurityConfig.java
+++ escuagest-api/src/main/java/com/escualos/SecurityConfig.java
@@ -1,27 +1,43 @@
 package com.escualos;
 
+import com.escualos.security.ReactiveKeycloakRealmRoleConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverter;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import reactor.core.publisher.Mono;
 
 @Configuration
 @EnableWebFluxSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableReactiveMethodSecurity
 public class SecurityConfig {
 
     @Bean
-    protected SecurityWebFilterChain configure(ServerHttpSecurity http) throws Exception {
+    public SecurityWebFilterChain configure(ServerHttpSecurity http) throws Exception {
         http
-            .csrf().disable()
-            .authorizeExchange()
-            .anyExchange().authenticated()
-            .and()
-            .oauth2ResourceServer()
-            .jwt();
+                .cors()
+                .and()
+                .authorizeExchange()
+                .anyExchange().authenticated()
+                .and()
+                .oauth2ResourceServer(oAuth2ResourceServerSpec -> oAuth2ResourceServerSpec.jwt(
+                    jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter())
+            ));
         return http.build();
+    }
+
+
+    private Converter<Jwt, ? extends Mono<? extends AbstractAuthenticationToken>> jwtAuthenticationConverter() {
+        ReactiveJwtAuthenticationConverter jwtConverter = new ReactiveJwtAuthenticationConverter();
+        jwtConverter.setJwtGrantedAuthoritiesConverter(new ReactiveKeycloakRealmRoleConverter());
+        return jwtConverter;
     }
 
 }
--- escuagest-api/src/main/java/com/escualos/api/testController.java
+++ escuagest-api/src/main/java/com/escualos/api/testController.java
@@ -1,22 +1,34 @@
 package com.escualos.api;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import reactor.core.publisher.Mono;
 
-import javax.annotation.security.RolesAllowed;
+import java.security.Principal;
 
 @Controller
+@RequestMapping
 public class testController {
 
     @GetMapping("/test/noRole")
-    @RolesAllowed("NO_ROLE")
-    public Mono<ResponseEntity<String>> noRole() {
+    public Mono<ResponseEntity<String>> noRole(Principal principal) {
         return Mono.just(ResponseEntity.ok("OK"));
     }
 
 
+    @GetMapping("/test/noRole2")
+    @PreAuthorize("isAuthenticated()")
+    public Mono<ResponseEntity<String>> noRole2(Principal principal) {
+        return Mono.just(ResponseEntity.ok("OK"));
+    }
+
+    @GetMapping("/test/noRole3")
+    @PreAuthorize("hasRole('NO_ROLE_2')")
+    public Mono<ResponseEntity<String>> noRole3(Principal principal) {
+        return Mono.just(ResponseEntity.ok("OK"));
+    }
 
 }
